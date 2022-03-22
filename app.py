b555621df038cbb597224e11b203a870f1da258e
jkurdziel <jakub.kurdziel@talkie.ai>
2022-03-22 20:54:07 +0100

    Create scraper class

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
-from bs4 import BeautifulSoup
-import requests
+from scraper import Scraper
 
-product_id = "99105003"
+product_id = "103735237"
+ceneo_scraper = Scraper(product_id)
+product_reviews = ceneo_scraper.get_all_opinion()
+print(product_reviews)
 
-def get_product_url(product_id):
-    return "https://www.ceneo.pl/"+ product_id + "#tab=reviews" 
-    
-
-
-page_html = requests.get(get_product_url(product_id))
-
-
-soup = BeautifulSoup(page_html.text, 'lxml')
-
-
-user_reviews = soup.find_all("div", class_="user-post__content")
-
-
-for review in user_reviews:
-    print(review.text)
-
-
+# todo recomendation with status  "Nie polecam" are not included in the result it must be fixed
---
+++ scraper.py
@@ -0,0 +1,88 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+class Scraper:
+
+    def __init__(self, product_id):
+        self.product_id = product_id
+
+    def get_all_opinion(self):
+        page_html = requests.get(self.get_product_url())
+        soup = BeautifulSoup(page_html.text, 'html.parser')
+        user_reviews = soup.find_all("div", class_="user-post__body")
+        reviews = []
+        for review in user_reviews:
+            review_details = self.get_review_details(review)
+            # remove comments from array
+            if review_details["author"] is None or review_details[
+                    'author'] == " Użytkownik Ceneo " or review_details[
+                        "recomendation"] is None:
+                continue
+            reviews.append(review_details)
+        return reviews
+
+    def get_product_url(self):
+        return "https://www.ceneo.pl/" + self.product_id + "#tab=reviews"
+
+    def get_review_details(self, review):
+        review_details = {}
+        review_details["author"] = get_value_for_element(
+            review, "span", "user-post__author-name")
+        review_details["comment_date"] = get_review_data(review)
+        review_details["purchase_date"] = get_purhase_date(review)
+        review_details["opinion"] = get_value_for_element(
+            review, "div", "user-post__text")
+        review_details["recomendation"] = get_value_for_element(
+            review, "em", "recommended")
+        review_details["score"] = get_value_for_element(
+            review, "span", "user-post__score-count")
+
+        like_button = review.find("button", class_="js_vote-yes")
+        if like_button is not None:
+            review_details["likes"] = like_button.find("span").text
+
+        dislike_button = review.find("button", class_="js_vote-no")
+        if dislike_button is not None:
+            review_details["dislikes"] = dislike_button.find("span").text
+
+        review_details["props"] = 0
+        review_details["cons"] = 0
+        features = review.find_all("div", class_="review-feature__col")
+
+        if (len(features) > 0):
+            childrens_props = features[0].findChildren("div", recursive=False)
+            review_details["props"] = len(childrens_props) - 1
+        if (len(features) > 1):
+            childrens_cons = features[1].findChildren("div", recursive=False)
+            review_details["cons"] = len(childrens_cons) - 1
+
+        return review_details
+
+
+def get_value_for_element(review, tag, attribute):
+    elemnet = review.find(tag, class_=attribute)
+    if elemnet is None or elemnet.text is None:
+        return None
+    return elemnet.text.replace("\n", " ")
+
+
+def get_size_of_element(review, tag, attribute):
+    elemnet = review.find_all(tag, class_=attribute)
+    if elemnet is not None:
+        return len(elemnet.text)
+    return 0
+
+
+def get_review_data(review):
+    elements = review.find_all("time")
+    if len(elements) == 0:
+        return None
+    return elements[0].get('datetime')
+
+
+def get_purhase_date(review):
+    elements = review.find_all("time")
+    if len(elements) < 2:
+        return None
+    return review.find_all("time")[1].get('datetime')
