54b6fb0963df362a5b4f512a80bae4d08402f84e
jkurdziel <jakub.kurdziel@talkie.ai>
2022-03-23 23:23:10 +0100

    handle routing

--- app.py
+++ app.py
@@ -1,25 +1,9 @@
 from scraper import Scraper
 from flask import Flask, render_template, request
 
-product_id = "103735237"
-ceneo_scraper = Scraper(product_id)
-product_reviews = ceneo_scraper.get_all_opinions()
+my_product_id = "103735237"
 
-product_reviews_as_string = ""
-for review in product_reviews:
-    output = ""
-    output += "Autor: " + review["author"] + "<br>"
-    output += "Opinia: " + review["opinion"] + "<br>"
-    output += "Ocena: " + review["score"] + "<br>"
-    output += "Recomendacja: " + str(review["recomendation"]) + "<br>"
-    output += "Data zakupu: " + str(review["purchase_date"]) + "<br>"
-    output += "Data opinii: " + str(review["comment_date"]) + "<br>"
-    output += "Likes: " + review["likes"] + "<br>"
-    output += "Dislikes: " + review["dislikes"] + "<br>"
-    output += "Props: " + str(review["props"]) + "<br>"
-    output += "Cons: " + str(review["cons"]) + "<br>"
-    output += "<br>"
-    product_reviews_as_string += (output + "<br> <br>")
+
 
 app = Flask(__name__, template_folder="templates")
 
@@ -29,15 +13,34 @@ def index():
     return render_template('index.html')
 
 
-@app.route(rule='/product/<product_id>', methods=['GET'])
-def product(product_id):
-    return '<pre> ' + product_id + ' </pre>'
+@app.route(rule='/product', methods=['GET'])
+def _product():
+    product_id = request.args.get('product_id')
+
+    print(product_id)
+    if product_id is None:
+        return render_template('product.html')
+    try:
+        ceneo_scraper = Scraper(product_id)
+        product_reviews = ceneo_scraper.get_all_opinions()
+        print(product_reviews)
+        return render_template('opinion.html', product_reviews=product_reviews)
+    except Exception as e:
+        print("Error: ", e)
+        return render_template('product.html', error=e)
+
+
 
 
 @app.route(rule='/about', methods=['GET'])
 def about():
     return render_template('about.html')
 
+
+
+@app.route(rule='/opinion', methods=['GET'])
+def opinion():     
+        return render_template('product.html', error="Aby przejść do strony opinii, wpisz id produktu.") 
 
 if __name__ == '__main__':
     app.run()
